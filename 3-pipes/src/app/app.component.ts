c75210033dad1024671e764dcd8c4087f4e04ed5
Gilbert Acosta <acostagilberto95@gmail.com>
2020-01-23 01:06:17 -0400

    pipe fecha

--- 3-pipes/src/app/app.component.ts
+++ 3-pipes/src/app/app.component.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  nombre = "Gilbert";
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  nombre: string = "Gilbert";
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  a: number = 0.234;
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: "logan",
+    clave: "wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise ( (resolve, reject) => {
+
+    setTimeout(() => resolve('llego la data!!'), 3500);
+
+  } );
+
+
 }
