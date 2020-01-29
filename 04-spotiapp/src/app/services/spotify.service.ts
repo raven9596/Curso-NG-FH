2b3c03dd7645a50a59fd8d77ad6437238afa6ce4
Gilberto Acosta <acostagilberto95@gmail.com>
2020-01-29 14:18:35 -0400

    obteniendo los datos del search seguir

--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +15,25 @@ export class SpotifyService {
   getNewReleases() {
     
     const headers = new HttpHeaders( {
-      'Authorization': 'Bearer BQDSwcR3u8Ad8lSUa12C_OpLNpzYxqrGgLRxf6OrjqT_fmcEYYJJRV7p4hyVnVwwYPOrw1mP_L9UItVApSo'
+      'Authorization': 'Bearer BQA1wmwwkoHX5aDFimi07nH_HFzFYss2XWPIhdi-Uy0fzHH_lDib_OH2_jGUPj3RD3-yDdj_6u0DgDF4Xlk'
     } );
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+           .pipe( map( data => {
+              return data['albums'].items;
+            }));
+  }
+ 
+  getArtista(termino: string) {
+
+    const headers = new HttpHeaders( {
+      'Authorization': 'Bearer BQA1wmwwkoHX5aDFimi07nH_HFzFYss2XWPIhdi-Uy0fzHH_lDib_OH2_jGUPj3RD3-yDdj_6u0DgDF4Xlk'
+    } );
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&offset=15`, { headers })
+    .pipe( map( data => {
+      return data['artists'].items;
+    }));
+
   }
 }
