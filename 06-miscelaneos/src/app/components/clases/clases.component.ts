fad05c2599bd753a9582a239a5251e2fcaae1c70
Gilberto Acosta <acostagilberto95@gmail.com>
2020-02-28 16:49:20 -0400

    Componentes y directivas angular

---
+++ 06-miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
