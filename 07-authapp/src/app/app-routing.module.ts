021a66d6be44edf2c3a659f9f8ccdcfd89e4e637
Gilberto Acosta <acostagilberto95@gmail.com>
2020-03-04 17:05:22 -0400

    Seguir en obtener perfil de usuario

--- 07-authapp/src/app/app-routing.module.ts
+++ 07-authapp/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'precios', component: PreciosComponent},
+  {path: 'protegida', component: ProtegidaComponent, canActivate: [AuthGuard]},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
