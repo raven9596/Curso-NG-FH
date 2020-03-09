95e8f07b54390406b44a87b8d8fbb0276bbbc0ec
Gilberto Acosta <acostagilberto95@gmail.com>
2020-03-09 15:52:26 -0400

    Login

---
+++ 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = 'acostagilberto95@gmail.com';
+  }
+
+
+}
