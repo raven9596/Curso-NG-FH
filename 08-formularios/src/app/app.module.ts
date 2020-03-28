aa5c84c1fb5b9b7ea3049b5a85bea09ba122bea9
Gilbert Acosta <acostagilberto95@gmail.com>
2020-03-28 19:45:53 -0400

    Seguir en formArray

--- 08-formularios/src/app/app.module.ts
+++ 08-formularios/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -20,6 +20,7 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
 
   ],
--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.crearFormulario();
+    this.cargarDataAlFormulario();
+
+
+  }
 
   ngOnInit(): void {
   }
 
-}
+  // Getters
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
+
+
+  crearFormulario() {
+    this.forma = this.fb.group({
+      nombre  : ['', [ Validators.required, Validators.minLength(5)] ],
+      apellido: ['', [ Validators.required, Validators.minLength(5)] ],
+      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      })
+    });
+
+  }
+
+  cargarDataAlFormulario() {
+    this.forma.setValue({
+      nombre: 'Gilbert',
+      apellido: 'Acosta',
+      correo: 'ga@gmail.com',
+      direccion: {
+        distrito: 'santo domingo',
+        ciudad: 'alma rosa'
+      }
+    });
+  }
+
+  guardar() {
+    console.log(this.forma);
+
+    if ( this.forma.invalid ) {
+
+      return Object.values( this.forma.controls ).forEach( control => {
+
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched() );
+        } else {
+
+          control.markAsTouched();
+        }
+      });
+    }
+
+    // Posteo de informacion
+    this.forma.reset({
+      nombre: 'Sin nombre'
+    });
+  }
+
+
+} // End Class
