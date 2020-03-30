4f2e41415a5c397152868a7d788d28b328872fd8
Gilbert Acosta <acostagilberto95@gmail.com>
2020-03-29 21:00:42 -0400

    seguir CRUD - Firebase

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,10 +11,12 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private validadores: ValidadoresService) {
 
     this.crearFormulario();
     this.cargarDataAlFormulario();
+    this.crearListeners();
 
 
   }
@@ -22,6 +25,10 @@ export class ReactiveComponent implements OnInit {
   }
 
   // Getters
+  get pasatiempos() {
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
   get nombreNoValido() {
     return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
   }
@@ -34,6 +41,10 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
 
+  get usuarioNoValido() {
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched;
+  }
+
   get distritoNoValido() {
     return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
   }
@@ -42,30 +53,70 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
   }
 
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return (pass1 === pass2 ) ? false : true;
+  }
+
+
 
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre  : ['', [ Validators.required, Validators.minLength(5)] ],
-      apellido: ['', [ Validators.required, Validators.minLength(5)] ],
-      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      nombre   : ['', [ Validators.required, Validators.minLength(5)] ],
+      apellido : ['', [ Validators.required, Validators.minLength(5), this.validadores.noAcosta ] ],
+      correo   : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      usuario  : ['', , this.validadores.existeUsuario ],
+      pass1    : ['', Validators.required],
+      pass2    : ['', Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
-        ciudad: ['', Validators.required]
-      })
+        ciudad  : ['', Validators.required]
+      }),
+      pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1', 'pass2')
     });
 
   }
 
+  crearListeners() {
+
+    this.forma.valueChanges.subscribe( valor => {
+      console.log(valor);
+    });
+
+    this.forma.statusChanges.subscribe( status => console.log({status}));
+
+  }
+
   cargarDataAlFormulario() {
-    this.forma.setValue({
+    // this.forma.setValue({
+    this.forma.reset({
       nombre: 'Gilbert',
-      apellido: 'Acosta',
+      apellido: 'Bocio',
       correo: 'ga@gmail.com',
+      pass1: '123',
+      pass2: '123',
       direccion: {
         distrito: 'santo domingo',
         ciudad: 'alma rosa'
       }
     });
+
+  }
+
+  agregarPasatiempo() {
+    this.pasatiempos.push( this.fb.control('', Validators.required))
+  }
+
+  borrarPasatiempo(i: number) {
+    this.pasatiempos.removeAt(i);
   }
 
   guardar() {
