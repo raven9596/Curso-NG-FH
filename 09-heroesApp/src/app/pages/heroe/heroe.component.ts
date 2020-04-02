62a5bec409e0df78d2fa1d4218ca9f19ab4ea131
Gilbert Acosta <acostagilberto95@gmail.com>
2020-04-01 22:36:46 -0400

    Configurando firebase seguir

---
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar( form: NgForm ) {
+
+    if ( form.invalid ) {
+      console.log('Formulario no valido');
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe);
+  }
+
+}
