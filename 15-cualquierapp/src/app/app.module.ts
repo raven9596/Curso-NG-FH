3f434097d4cc88ecee89d6655442216e4da5a183
Gilbert Acosta <acostagilberto95@gmail.com>
2020-04-28 02:20:07 -0400

    despliegue de apps

---
+++ 15-cualquierapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { Pagina2Component } from './pages/pagina2/pagina2.component';
+import { Pagina3Component } from './pages/pagina3/pagina3.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
